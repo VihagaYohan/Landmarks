29e2c3aee33e91b21a82bab6e057a4077d01cf90
Vihaga Yohan <35211294+VihagaYohan@users.noreply.github.com>
2024-07-28 06:31:24 +0530

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Yohan on 2024-07-28.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
